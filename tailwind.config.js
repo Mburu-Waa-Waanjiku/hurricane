56277430549fe64e1d8815c2549fe82aeda7317c
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2023-06-29 17:49:16 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       screens: {
+        'xxsm': '408px',
         'xsm': '600px',
         'mlg': '990px'
       }

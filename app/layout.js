c3ff2b7781b9465e106edde2c3826885d4b0079d
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2024-10-21 10:31:55 +0300

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ import '../styles/globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="ehyifMyuzZjrrJmMdqaTCAsoDerpHiSeejh92XkAQPI" />
+      </head>
       <body>
         <StateProvider>
           <Header />

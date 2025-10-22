4f5c554e0b6499f0aca3e187f525c302df4d9fd1
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2025-10-22 10:23:06 +0300

    Add files via upload

---
+++ app/blogs/rent-an-online-store/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RentaShop from '../../../components/RentaShop'
+
+function page() {
+  return (
+    <>
+      <RentaShop/>
+    </>
+  )
+}
+
+export default page

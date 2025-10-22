91e705e9785e4e195794934392a0c38643102b0d
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2025-10-22 10:59:57 +0300

    Add files via upload

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default async function sitemap() {
+    // Base URL of your site (update this)
+    const baseUrl = "https://hurricaneteck.co.ke";
+  
+    // Example: Static pages
+    const staticPages = [
+      "",
+      "/virtual-tours",
+      "/virtual-tours/winsper-lands-sagana-phase-1",
+      "/shop",
+      "/services/marketing",
+      "/services/web-design",
+      "/blogs/get-a-website-from-the-best-web-designers-in-kenya-nairobi",
+      "/blogs/rent-an-online-store",
+      "/blogs/top-3-best-website-developer-companies-in-ruiru-2025",
+    ];
+
+    return routes;
+  }
+  
